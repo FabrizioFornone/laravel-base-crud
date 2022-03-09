ad1d5229ad65eab3d5dc40b5dba83c625f1514d2
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-03-09 17:06:57 +0100

    exercise done, bonus done

--- resources/views/partials/deleteBtn.blade.php
+++ resources/views/partials/deleteBtn.blade.php
@@ -1,6 +1,21 @@
-<form action="{{ route('comics.destroy', $comic->id) }}" method="post">
+<form action="{{ route('comics.destroy', $comic->id) }}" method="post" class="delete_form_{{ $comic->id }}">
     @csrf
     @method("delete")
 
     <button type="submit">Elimina</button>
 </form>
+
+
+<script>
+    const delete_{{ $comic->id }} = document.querySelector(".delete_form_{{ $comic->id }}")
+  
+    delete_{{ $comic->id }}.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const modal = confirm("Sei sicuro?");
+  
+      if (modal) {
+        delete_{{ $comic->id}}.submit();
+      }
+    })
+</script>
